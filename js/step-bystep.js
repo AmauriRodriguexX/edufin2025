56198aa6a9733a2e3eaa26c6215f316d08045910
AmauriRodriguexX <amauri.rodriguez@t2o.com>
2025-06-16 17:27:37 -0600

    fdsfdsfdsfds

--- js/step-bystep.js
+++ js/step-bystep.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-     const initialStepId = 'step-8-final'; // ← cámbialo al ID que quieras mostrar
+     const initialStepId = 'step-10-final'; // ← cámbialo al ID que quieras mostrar
      document.querySelectorAll(".step-content").forEach(step => step.classList.add("d-none"));
      const initialStep = document.getElementById(initialStepId);
      if (initialStep) initialStep.classList.remove("d-none");
