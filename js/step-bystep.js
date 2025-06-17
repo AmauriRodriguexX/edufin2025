67179dfe46646b9db02f308452a4f95d07147df5
AmauriRodriguexX <amauri.rodriguez@t2o.com>
2025-06-16 18:55:51 -0600

    asdsads

--- js/step-bystep.js
+++ js/step-bystep.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-     const initialStepId = 'step-11-correcto'; // ← cámbialo al ID que quieras mostrar
+     const initialStepId = 'step-10.7'; // ← cámbialo al ID que quieras mostrar
      document.querySelectorAll(".step-content").forEach(step => step.classList.add("d-none"));
      const initialStep = document.getElementById(initialStepId);
      if (initialStep) initialStep.classList.remove("d-none");
